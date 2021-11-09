0613651a43705364c067123e3419eea05f9ab052
MrGiaoegg <91585367+MrGiaoegg@users.noreply.github.com>
2021-11-09 23:07:19 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Made by Egg
+## Github Account: MrGiaoegg
+## A Game of Python
+## Source: 
+## Enjoy
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='文字游戏1.py',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='桀',
+    author_email='',
+    description=''
+)
